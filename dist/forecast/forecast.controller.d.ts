5fdccce27779ff6f910c89f9ba4ff8685b52042b
jagannathsrs <jagannathsrs@gmail.com>
2020-05-25 09:37:56 -0400

    removed serverless

--- dist/forecast/forecast.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ForecastService } from './forecast.service';
-export declare class ForecastController {
-    private readonly ForecastService;
-    constructor(ForecastService: ForecastService);
-    getForecast(location: string, lat: number, lon: number): Promise<any>;
-}
--- dist/forecast/forecast.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@nestjs/common';
-export declare class ForecastService {
-    private http;
-    constructor(http: HttpService);
-    getLatLong(location?: string, lat?: number, lon?: number): Promise<any>;
-    getForecast(locationObject: any): Promise<any>;
-    extractData(response: any): Promise<[unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown]>;
-    getMoonIllumination(date: any): Promise<string>;
-}
